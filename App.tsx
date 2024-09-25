dc428657b08136afcbb65fe3970e715f6ab99bb9
Markus <Markus147@live.se>
2024-09-25 20:49:47 +0200

    Get items and render a few for testing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Item, fetchItems } from "./fetchItems";
+
+// const baseUrl = "https://bindingofisaacrebirth.fandom.com/";
 
 export default function App() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const getItems = async () => {
+      const items = await fetchItems();
+      setItems(items);
+      // console.log("Fetched items in App:", items);
+    };
+
+    getItems();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {items.slice(0, 10).map((item) => (
+        <View key={item.title}>
+          <Image
+            source={{ uri: item.image }}
+            style={{ width: 48, height: 48 }}
+          />
+          <Text>{item.title}</Text>
+          {/* <Text>{baseUrl + item.link}</Text> */}
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
