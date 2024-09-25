f8ea89a93d8fe12d16c3514f53eccd31e9ac07cf
Markus <Markus147@live.se>
2024-09-25 20:47:56 +0200

    Implemented web scraping for TBOI items from wiki

---
+++ fetchItems.ts
@@ -0,0 +1,32 @@
+import cheerio from "cheerio-without-node-native";
+
+export interface Item {
+  title: string;
+  image: string;
+  link: string;
+}
+
+export const fetchItems = async (): Promise<Item[]> => {
+  try {
+    const response = await fetch(
+      "https://bindingofisaacrebirth.fandom.com/wiki/Collection_Page_(Repentance)"
+    );
+
+    const htmlString = await response.text();
+    const $ = cheerio.load(htmlString);
+
+    const itemsArray: Item[] = Array.from($("td > span.tooltip")).map(
+      (element: any) => ({
+        title: $(element).find("a").attr("title"),
+        image: $(element).find("img").attr("data-src"),
+        link: $(element).find("a").attr("href"),
+      })
+    );
+
+    // console.log("Items Array:", itemsArray);
+    return itemsArray;
+  } catch (error) {
+    console.error("Error fetching Isaac items:", error);
+    return [];
+  }
+};
