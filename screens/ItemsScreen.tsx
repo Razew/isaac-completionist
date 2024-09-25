220298c30ea1dbe14a27ff3da177dfa8c3b3ed54
Markus <Markus147@live.se>
2024-09-25 21:49:13 +0200

    Increased number of columns

--- screens/ItemsScreen.tsx
+++ screens/ItemsScreen.tsx
@@ -1,13 +1,14 @@
 import { memo, useEffect, useState } from "react";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, StyleSheet, View } from "react-native";
 import fetchItems, { Item as ItemProps } from "../utils/fetchItems";
 
 // const baseUrl = "https://bindingofisaacrebirth.fandom.com/";
 
+// TODO: Add react-native-tooltip?
 const Item = memo(({ item }: { item: ItemProps }) => (
   <View key={item.title}>
     <Image source={{ uri: item.image }} style={{ width: 48, height: 48 }} />
-    <Text>{item.title}</Text>
+    {/* <Text>{item.title}</Text> */}
   </View>
 ));
 
@@ -29,6 +30,7 @@ export default function ItemsScreen() {
         data={items}
         renderItem={({ item }) => <Item item={item} />}
         keyExtractor={(item, index) => `${item.title}-${index}`}
+        numColumns={6}
       />
     </View>
   );
