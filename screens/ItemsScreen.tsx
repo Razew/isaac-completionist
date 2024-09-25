5dbaf75ffb0f0ab879b249511aaec7cf283fc30f
Markus <Markus147@live.se>
2024-09-25 21:57:31 +0200

    Added marginTop so as to not overlap with StatusBar

--- screens/ItemsScreen.tsx
+++ screens/ItemsScreen.tsx
@@ -1,5 +1,12 @@
 import { memo, useEffect, useState } from "react";
-import { FlatList, Image, StyleSheet, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
 import fetchItems, { Item as ItemProps } from "../utils/fetchItems";
 
 // const baseUrl = "https://bindingofisaacrebirth.fandom.com/";
@@ -7,7 +14,7 @@ import fetchItems, { Item as ItemProps } from "../utils/fetchItems";
 // TODO: Add react-native-tooltip?
 const Item = memo(({ item }: { item: ItemProps }) => (
   <View key={item.title}>
-    <Image source={{ uri: item.image }} style={{ width: 48, height: 48 }} />
+    <Image source={{ uri: item.image }} style={{ width: 55, height: 55 }} />
     {/* <Text>{item.title}</Text> */}
   </View>
 ));
@@ -25,22 +32,21 @@ export default function ItemsScreen() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <FlatList
         data={items}
         renderItem={({ item }) => <Item item={item} />}
         keyExtractor={(item, index) => `${item.title}-${index}`}
         numColumns={6}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
