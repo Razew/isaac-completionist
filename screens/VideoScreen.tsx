2f34911d5cdc57a86255ef61054048b40138158e
Markus <Markus147@live.se>
2024-09-30 23:03:52 +0200

    Added skip video and redirection to home navigator upon skip/finish

--- screens/VideoScreen.tsx
+++ screens/VideoScreen.tsx
@@ -1,10 +1,18 @@
-import { ResizeMode, Video } from "expo-av";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { AVPlaybackStatus, ResizeMode, Video } from "expo-av";
 import * as ScreenOrientation from "expo-screen-orientation";
-import { useEffect, useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { StyleSheet, TouchableWithoutFeedback, View } from "react-native";
+import { RootStackParamList } from "../navigators/RootStackNavigator";
 
-export default function VideoScreen() {
-  const video = useRef(null);
+type Props = NativeStackScreenProps<RootStackParamList, "Video">;
+
+// TODO: Hide statusbar/buttons when video playing?
+export default function VideoScreen({ navigation }: Props) {
+  const video = useRef<Video>(null);
+  const [status, setStatus] = useState<AVPlaybackStatus>(
+    {} as AVPlaybackStatus
+  );
 
   useEffect(() => {
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
@@ -13,19 +21,43 @@ export default function VideoScreen() {
     };
   }, []);
 
+  const handlePlaybackStatusUpdate = async (status: AVPlaybackStatus) => {
+    setStatus(status);
+
+    if (status.isLoaded) {
+      if (status.positionMillis < 4000) {
+        await video.current?.setPositionAsync(4000);
+      }
+      if (status.didJustFinish) {
+        ScreenOrientation.unlockAsync();
+        navigation.navigate("HomeNavigator");
+      }
+    }
+  };
+
+  const handleExit = async () => {
+    if (video.current) {
+      await video.current.stopAsync();
+    }
+    ScreenOrientation.unlockAsync();
+    navigation.navigate("HomeNavigator");
+  };
+
   return (
-    <View style={styles.container}>
-      <Video
-        ref={video}
-        source={{
-          uri: "https://video.akamai.steamstatic.com/store_trailers/2034156/movie480.webm?t=1447363402",
-        }}
-        resizeMode={ResizeMode.CONTAIN}
-        shouldPlay
-        style={styles.video}
-        // onPlaybackStatusUpdate={}
-      />
-    </View>
+    <TouchableWithoutFeedback onPress={handleExit}>
+      <View style={styles.container}>
+        <Video
+          ref={video}
+          source={{
+            uri: "https://video.akamai.steamstatic.com/store_trailers/2034156/movie480.webm?t=1447363402",
+          }}
+          resizeMode={ResizeMode.CONTAIN}
+          shouldPlay
+          style={styles.video}
+          onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
