8ba5a93577b19a6c25282512b39ef53304d64a87
Markus <Markus147@live.se>
2024-10-01 20:39:27 +0200

    Refactored Item image to use dynamic size

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,25 +1,29 @@
 import { Image } from "expo-image";
 import { memo } from "react";
-import { Pressable, StyleSheet, View } from "react-native";
+import { Pressable, View } from "react-native";
 import { Tooltip } from "react-native-paper";
 import { Item as ItemProps } from "../utils/fetchItems";
 
-const Item = ({ item, onPress }: { item: ItemProps; onPress: () => void }) => (
+const Item = ({
+  item,
+  onPress,
+  size,
+}: {
+  item: ItemProps;
+  onPress: () => void;
+  size: number;
+}) => (
   <Tooltip title={item.title} leaveTouchDelay={200}>
     <Pressable onPress={onPress}>
       <View>
-        <Image source={{ uri: item.image }} style={styles.itemImage} />
+        <Image
+          source={{ uri: item.image }}
+          style={{ width: size, height: size }}
+        />
       </View>
     </Pressable>
   </Tooltip>
 );
-
-const styles = StyleSheet.create({
-  itemImage: {
-    width: 55,
-    height: 55,
-  },
-});
 
 export default memo(
   Item,
