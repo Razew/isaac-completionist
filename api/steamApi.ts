2ff766058afd711662f95ec1bd0909485f57f945
Markus <Markus147@live.se>
2024-10-02 17:54:22 +0200

    Fetch all achievements from steam's api

---
+++ api/steamApi.ts
@@ -0,0 +1,24 @@
+import { API_KEY } from "./steamApiKey";
+
+export type AchievementData = {
+  name: string;
+  displayName: string;
+  description: string;
+  icon: string;
+  icongray: string;
+};
+
+export const fetchAllAchievements = async (): Promise<AchievementData[]> => {
+  const url = `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${API_KEY}&appid=250900`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.game.availableGameStats.achievements;
+};
+
+//   "name": "1",
+//   "defaultvalue": 0,
+//   "displayName": "Magdalene",
+//   "hidden": 0,
+//   "description": "Unlocked a new character.",
+//   "icon": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/250900/a36d7e92df7e991758907a75dfa55d36b52548c4.jpg",
+//   "icongray": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/apps/250900/fc5c40f3429120652211c9e95570752ba5810f22.jpg"
--- screens/AchievementsScreen.tsx
+++ screens/AchievementsScreen.tsx
@@ -1,11 +1,24 @@
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 import { Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AchievementData, fetchAllAchievements } from "../api/steamApi";
 
 export default function AchievementsScreen() {
+  const [achievements, setAchievements] = useState<AchievementData[]>([]);
+
+  useEffect(() => {
+    async function fetcher() {
+      const result = await fetchAllAchievements();
+      setAchievements(result);
+    }
+    fetcher();
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Achievements Screen</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
