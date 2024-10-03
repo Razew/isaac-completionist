3247d9ced2e17717e8ef41e927089800c4ea282e
Markus <Markus147@live.se>
2024-10-03 13:25:21 +0200

    Removed apiKeyExists state in favour of checking achievements.length

--- screens/AchievementsScreen.tsx
+++ screens/AchievementsScreen.tsx
@@ -6,38 +6,35 @@ import { StyleSheet } from "react-native";
 import { Button, Surface, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AchievementData, fetchAllAchievements } from "../api/steamApi";
+import AchievementCard from "../components/AchievementCard";
 import { RootStackParamList } from "../navigators/RootStackNavigator";
 import { TabParamList } from "../navigators/TabNavigator";
-import { checkApiKeyExists } from "./SettingsScreen";
 
 type Props = CompositeScreenProps<
   BottomTabScreenProps<TabParamList, "Achievements">,
   NativeStackScreenProps<RootStackParamList>
 >;
 
-// FIXME: Recheck api key exists if necessary (if key has been added after first navigate to screen)
 export default function AchievementsScreen({ navigation }: Props) {
   const [achievements, setAchievements] = useState<AchievementData[]>([]);
-  const [apiKeyExists, setApiKeyExists] = useState(false);
+
+  const renderItem = ({ achievement }: { achievement: AchievementData }) => (
+    <AchievementCard achievement={achievement} />
+  );
 
   useEffect(() => {
     async function fetcher() {
-      const keyExists = await checkApiKeyExists();
-      setApiKeyExists(keyExists);
-
-      if (keyExists) {
-        const result = await fetchAllAchievements();
-        setAchievements(result);
-      }
+      const result = await fetchAllAchievements();
+      setAchievements(result);
     }
     fetcher();
   }, []);
 
-  if (!apiKeyExists) {
+  if (achievements.length === 0) {
     return (
       <SafeAreaView style={styles.container}>
         <Surface style={styles.messageContainer} elevation={4}>
-          <Text style={styles.title}>No API Key Found</Text>
+          <Text style={styles.title}>No Valid API Key Found</Text>
           <Text style={styles.message}>
             To access the achievements functionality, please add a Steam API key
             in the settings menu.
@@ -57,9 +54,10 @@ export default function AchievementsScreen({ navigation }: Props) {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Achievements Screen</Text>
-      {achievements.map((a) => (
+      {renderItem({ achievement: achievements[0] })}
+      {/* {achievements.map((a) => (
         <Text key={a.name}>{a.displayName}</Text>
-      ))}
+      ))} */}
     </SafeAreaView>
   );
 }
