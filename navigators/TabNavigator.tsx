e9ff6997ff5a5662892fb4293b843f997010e1b4
Markus <Markus147@live.se>
2024-10-04 17:38:52 +0200

    With react-native-paper's bottom navigation bar

--- navigators/TabNavigator.tsx
+++ navigators/TabNavigator.tsx
@@ -1,5 +1,7 @@
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { CommonActions } from "@react-navigation/native";
+import { BottomNavigation } from "react-native-paper";
 import AchievementsScreen from "../screens/AchievementsScreen";
 import HomeScreen from "../screens/HomeScreen";
 import ItemsScreen from "../screens/ItemsScreen";
@@ -15,11 +17,55 @@ const Tab = createBottomTabNavigator<TabParamList>();
 // TODO: Adjust placing of text relative to icon
 export default function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={{ headerShown: false }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.title;
+
+            return label;
+          }}
+        />
+      )}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
+          tabBarLabel: "Home",
           tabBarIcon: ({ size, color }) => (
             <MaterialCommunityIcons name="home" size={size} color={color} />
           ),
@@ -29,6 +75,7 @@ export default function TabNavigator() {
         name="Items"
         component={ItemsScreen}
         options={{
+          tabBarLabel: "Items",
           tabBarIcon: ({ size, color }) => (
             <MaterialCommunityIcons
               name="treasure-chest"
@@ -42,6 +89,7 @@ export default function TabNavigator() {
         name="Achievements"
         component={AchievementsScreen}
         options={{
+          tabBarLabel: "Achievements",
           tabBarIcon: ({ size, color }) => (
             <MaterialCommunityIcons name="trophy" size={size} color={color} />
           ),
