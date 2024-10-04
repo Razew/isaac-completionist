1573ed6ded146ccc3f6437a1766bed85bf99efa8
Markus <Markus147@live.se>
2024-10-04 17:02:17 +0200

    Replaced missing api key error with dialog window on empty list

--- screens/AchievementsScreen.tsx
+++ screens/AchievementsScreen.tsx
@@ -3,7 +3,7 @@ import { CompositeScreenProps, useFocusEffect } from "@react-navigation/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useCallback, useRef, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
-import { Button, Surface, Text } from "react-native-paper";
+import { Button, Dialog, Portal, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AchievementData, fetchAllAchievements } from "../api/steamApi";
 import AchievementCard from "../components/AchievementCard";
@@ -17,36 +17,46 @@ type Props = CompositeScreenProps<
 
 export default function AchievementsScreen({ navigation }: Props) {
   const [achievements, setAchievements] = useState<AchievementData[]>([]); // Hade eventuellt kunnat ta bort denna och enbart använda useRef
-  const [hasError, setHasError] = useState(false);
+  const [visible, setVisible] = useState(false);
   const achievementsRef = useRef<AchievementData[]>([]);
 
   const renderItem = ({ item }: { item: AchievementData }) => (
     <AchievementCard achievement={item} />
   );
 
-  // Hade nog kunnat byta ut detta mot en 'Dialog' från Paper
-  const ApiErrorMessage = () => (
-    <Surface style={styles.messageContainer} elevation={4}>
-      <Text style={styles.title}>No Valid API Key Found</Text>
-      <Text style={styles.message}>
-        To access the achievements functionality, please add a Steam API key in
-        the settings menu.
-      </Text>
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate("Settings")}
-        buttonColor="#721c24"
-      >
-        Go to Settings
-      </Button>
-    </Surface>
+  const handleDismiss = () => {
+    setVisible(false), navigation.goBack();
+  };
+
+  const ApiKeyAlert = () => (
+    <Portal>
+      <Dialog visible={visible} onDismiss={handleDismiss}>
+        <Dialog.Title>API Key Required</Dialog.Title>
+        <Dialog.Content>
+          <Text variant="bodyMedium">
+            To access Steam achievements, please add a Steam API key in the
+            settings menu.
+          </Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={handleDismiss}>Go back</Button>
+          <Button
+            onPress={() => {
+              setVisible(false), navigation.navigate("Settings");
+            }}
+          >
+            Go to Settings
+          </Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
   );
 
   const fetchAchievements = async () => {
-    setHasError(false);
+    setVisible(false);
     const result = await fetchAllAchievements();
     if (result.length === 0) {
-      setHasError(true);
+      setVisible(true);
     } else {
       setAchievements(result);
       achievementsRef.current = result;
@@ -65,21 +75,18 @@ export default function AchievementsScreen({ navigation }: Props) {
 
   return (
     <SafeAreaView style={styles.container}>
-      {hasError || achievements.length === 0 ? (
-        <ApiErrorMessage />
-      ) : (
-        <FlatList
-          ListHeaderComponent={
-            <Text style={styles.listHeader}>All Achievements</Text>
-          }
-          data={achievements}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.name}
-          initialNumToRender={20}
-          contentContainerStyle={styles.listContentContainer}
-          style={{ width: "100%" }}
-        />
-      )}
+      <FlatList
+        ListHeaderComponent={
+          <Text style={styles.listHeader}>All Achievements</Text>
+        }
+        data={achievements}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.name}
+        initialNumToRender={20}
+        contentContainerStyle={styles.listContentContainer}
+        ListEmptyComponent={ApiKeyAlert}
+        style={{ width: "100%" }}
+      />
     </SafeAreaView>
   );
 }
@@ -94,26 +101,6 @@ const styles = StyleSheet.create({
   },
   listContentContainer: {
     paddingHorizontal: 5,
-  },
-  messageContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#f8d7da",
-    borderRadius: 8,
-    padding: 20,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#721c24",
-    marginBottom: 10,
-  },
-  message: {
-    fontSize: 16,
-    color: "#721c24",
-    textAlign: "center",
-    marginBottom: 20,
   },
   listHeader: {
     fontSize: 26,
